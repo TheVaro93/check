8bc8a05d1c48b097d2a5bfd3c22f9b67c6612fe5
TheVaro93 <adrien.virorello@gmail.com>
2026-06-15 22:47:59 +0200

    init commit sans le html juste le fonctionnement de base pr test

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+const linkId = urlParams.get('id');
+
+
+const BACKEND_URL = "jle fous apres";
+
+if (!linkId) {
+    document.getElementById('status').innerText = "err mauvais lien.";
+} else {
+    fetch(BACKEND_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id_lien: linkId })
+    })
+        .then(() => {
+            document.getElementById('status').innerText = "Vérification réussie !";
+        })
+        .catch(() => {
+            document.getElementById('status').innerText = "err connexion au serveur de verif";
+        });
+}
