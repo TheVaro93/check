d680a374962ffb69b0c2170a7aa845b990ef78d5
TheVaro93 <adrien.virorello@gmail.com>
2026-06-16 17:58:18 +0200

    mise en page, correction de/des variables pr toutes les renommer de la meme facon et une meilleure version des messages de fin

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const urlParams = new URLSearchParams(window.location.search);
 const linkId = urlParams.get('id');
 const BACKEND_URL = "https://activism-suggest-probation.ngrok-free.dev/save-data";
@@ -6,18 +5,20 @@ const BACKEND_URL = "https://activism-suggest-probation.ngrok-free.dev/save-data
 if (!linkId) {
     document.getElementById('status').innerText = "err mauvais lien.";
 } else {
+    const donnéesAEnvoyer = { id_lien: linkId };
+
     fetch(BACKEND_URL, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "ngrok-skip-browser-warning": "any-value"
         },
-        body: JSON.stringify(donnees)
+        body: JSON.stringify(donnéesAEnvoyer)
     })
         .then(() => {
-            document.getElementById('status').innerText = "Vérification réussie !";
+            document.getElementById('status').innerText = "Vérification réussie ! Vous pouvez retourner à votre activité précédente.";
         })
         .catch(() => {
-            document.getElementById('status').innerText = "err connexion au serveur de verif";
+            document.getElementById('status').innerText = "Erreur de connexion au serveur";
         });
 }
